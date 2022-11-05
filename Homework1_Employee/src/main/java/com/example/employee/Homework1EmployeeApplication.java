5ad9db2f543e89c113ddf0724af07565cde3eb0f
radmanlo <radman.lotfiazar@gmail.com>
2022-11-05 19:12:01 +0300

    Homework1_Employee
    First homework project employee is created

---
+++ Homework1_Employee/src/main/java/com/example/employee/Homework1EmployeeApplication.java
@@ -0,0 +1,13 @@
+package com.example.employee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Homework1EmployeeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Homework1EmployeeApplication.class, args);
+	}
+
+}
